2ad3e28474b6acdf2547f734bf3f7f15739c2aa4
snftp <ale.drow@yandex.ru>
2026-04-17 18:25:46 +0300

    Modified for Altera Cyclone files and cleanup repository

--- labs/1_basics/1_01_and_or_not_xor_de_morgan/lab_top.sv
+++ labs/1_basics/1_01_and_or_not_xor_de_morgan/lab_top.sv
@@ -73,11 +73,11 @@ module lab_top
     wire a = key [0];
     wire b = key [1];
 
-    wire result = a ^ b;
+    // wire result = a ^ b;
 
-    assign led [0] = result;
+    // assign led [0] = result;
+    // assign led [1] = key [0] ^ key [1];
 
-    assign led [1] = key [0] ^ key [1];
     // assign led [2] = a ^ b;
 
     //------------------------------------------------------------------------
@@ -103,18 +103,27 @@ module lab_top
     // If led [2] is not available on your board,
     // comment out the code above and reuse led [0].
 
-    // assign led [2] =
+    // assign led [2] = a & b;
 
     // Exercise 2: Change the code below.
     // Assign to led [3] the result of XOR operation
     // without using "^" operation.
     // Use only operations "&", "|", "~" and parenthesis, "(" and ")".
 
-    // assign led [3] =
+    // assign led [3] = (a | b) & (~ (a & b));
 
     // Exercise 3: Create an illustration to De Morgan's laws:
     //
     // ~ (a & b) == ~ a | ~ b
     // ~ (a | b) == ~ a & ~ b
+
+    logic left, right;
+    // assign left  = ~ (a & b);
+    // assign right = ~ a | ~ b;
+    // assign led [0] = ~ (left ^ right);
+
+    assign left = ~ (a | b);
+    assign right = ~ a & ~ b;
+    assign led [0] = ~ (left ^ right);
 
 endmodule
