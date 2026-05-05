5c1edf1d98760b076b80ee04c50c025c233eb53d
snftp <ale.drow@yandex.ru>
2026-05-06 02:20:06 +0300

    hex counter implemented on semisegmention indicators with incrementation speed controll

--- labs/1_basics/1_09_hex_counter/lab_top.sv
+++ labs/1_basics/1_09_hex_counter/lab_top.sv
@@ -80,13 +80,38 @@ module lab_top
     localparam min_period = clk_mhz * 1000 * 1000 / 50,
                max_period = clk_mhz * 1000 * 1000 *  3;
 
+    wire halve = | key[0];
+    wire double  = | key[1];
+
+    logic halve_key_r;
+    logic double_key_r;
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+        begin
+            halve_key_r <= '0;
+            double_key_r <= '0;
+        end
+        else
+        begin
+            halve_key_r <= halve;
+            double_key_r  <= double;
+        end
+
+    wire halve_key_pressed = ~ halve & halve_key_r;
+    wire double_key_pressed  = ~ double & double_key_r;
+
     always_ff @ (posedge clk or posedge rst)
         if (rst)
             period <= 32' ((min_period + max_period) / 2);
-        else if (key [0] & period != max_period)
-            period <= period + 32'h1;
-        else if (key [1] & period != min_period)
-            period <= period - 32'h1;
+        else if (halve_key_pressed & period != max_period)
+            period <= period * 32'h2;
+        else if (double_key_pressed & period != min_period)
+            period <= period / 32'h2;
+
+    /* key[0] увеличивает значение периода. Поскольку мы ждем дольше, это замедляет счетчик.
+       key[1] уменьшает период. Мы ждем меньше, это ускоряет счетчик.
+       При сбросе устанавливается среднее значение скорости. */
 
     logic [31:0] cnt_1;
 
@@ -98,6 +123,10 @@ module lab_top
         else
             cnt_1 <= cnt_1 - 1'd1;
 
+    /* Это вычитающий счетчик. Он берет текущее значение period
+       и каждую итерацию clk делает −1. Когда он доходит до нуля,
+       он снова загружает значение из period. */
+
     logic [31:0] cnt_2;
 
     always_ff @ (posedge clk or posedge rst)
@@ -108,20 +137,35 @@ module lab_top
 
     assign led = cnt_2;
 
+    /* число, которое мы считаем.
+       он увеличивается на единицу только в тот момент,
+       когда таймер cnt_1 доходит до нуля
+       assign led = cnt_2 выводит это число в двоичном виде на светодиоды платы */
+
     //------------------------------------------------------------------------
 
-    // 4 bits per hexadecimal digit
-    localparam w_display_number = w_digit * 4;
+    // 4 bits per hexadecimal digit - Для отображения одной шестнадцатеричной цифры (от 0 до F) требуется ровно 4 бита данных
+    localparam w_display_number = w_digit * 4; // гарантирует, что модуль индикации получит ровно столько бит, сколько он сможет физически отобразить
 
     seven_segment_display # (w_digit) i_7segment
     (
-        .clk      ( clk                       ),
-        .rst      ( rst                       ),
-        .number   ( w_display_number' (cnt_2) ),
+        .clk      ( clk                       ), // подключение тактового сигнала
+        .rst      ( rst                       ), // подключение сброса
+        .number   ( w_display_number' (cnt_2) ), // передаем значение счетчика cnt_2
         .dots     ( w_digit' (0)              ),
-        .abcdefgh ( abcdefgh                  ),
-        .digit    ( digit                     )
+        .abcdefgh ( abcdefgh                  ), // Выход на сегменты (катоды)
+        .digit    ( digit                     )  // Выход на общие аноды
     );
+
+    /* Преобразует 32-битное двоичное число cnt_2 в сигналы
+       для управления сегментами индикатора.
+       Внутри этого модуля уже написан код, который берет число и
+       по очереди зажигает нужные «палочки». не нужно писать
+       assign abcdefgh, потому что порты модуля i_7segment напрямую
+       соединены с выходными портами вашего lab_top.
+       Порты abcdefgh и digit, описанные в заголовке module lab_top,
+       жестко привязаны к физическим ножкам микросхемы,
+       к которым припаян индикатор. */
 
     //------------------------------------------------------------------------
 
