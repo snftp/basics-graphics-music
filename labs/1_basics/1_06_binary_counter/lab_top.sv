10ed667bbc702d9753f1dbffed018c59d29b47db
snftp <ale.drow@yandex.ru>
2026-05-05 04:04:56 +0300

    Binary counter as a blink frequancy controller test, comment regarding some functions and common logic, incrementer and decrementer on different keys and incrementers on different leds and keys tasks

--- labs/1_basics/1_06_binary_counter/lab_top.sv
+++ labs/1_basics/1_06_binary_counter/lab_top.sv
@@ -76,7 +76,22 @@ module lab_top
 
     localparam w_cnt = $clog2 (clk_mhz * 1000 * 1000);
 
-    logic [w_cnt - 1:0] cnt;
+    /* localparam — это локальная константа модуля.
+    В отличие от обычного parameter, её нельзя переопределить извне,
+    используется для вычисления внутренних констант, которые зависят
+    от других параметров (например, от входной частоты),
+    чтобы не вписывать конкретные числа вручную.
+
+    Выражение clk_mhz * 1000 * 1000 переводит частоту из Мегагерц в Герцы.
+    Герц (Hz) — это количество тактов (циклов) в одну секунду.
+    Если входная частота составляет 50 MHz, то за одну секунду произойдет 50 000 000 тактов.
+
+    Системная функция $clog2 (ceiling log2) вычисляет двоичный логарифм
+    с округлением вверх. В цифровой электронике это единственный способ
+    определить, сколько бит (физических триггеров) нужно выделить для
+    хранения конкретного числа. те w_cnt вычислет разрядность количества тактов */
+
+    logic [w_cnt - 1:0] cnt; // стандартный способ определить диапазон индексов шины
 
     always_ff @ (posedge clk or posedge rst)
         if (rst)
@@ -84,7 +99,23 @@ module lab_top
         else
             cnt <= cnt + 1'd1;
 
-    assign led = cnt [$left (cnt) -: w_led];
+    /* Бит cnt[ меняет свое состояние (из 0 в 1 или из 1 в 0) каждый такт
+    Биты после нулевого меняются благодаря правилам двоичной арифметики.
+    Когда младший бит уже равен 1 и к нему прибавляется еще 1,
+    происходит перенос разряда в следующий бит.
+    Бит cnt[1] меняется в 2 раза реже, чем cnt[0], а cnt[2] — в 4 раза реже */
+
+    assign led = cnt [$left (cnt) -: w_led]; // самый левый слайс cnt то есть самый низкочастотный
+    assign led = cnt [$right (cnt) +: w_led]; // самый правый слайс значит самая высокая частота короче ниче не видно
+    assign led = cnt [($left (cnt) - 4) -: (w_led - 1)]; // уже различимо глазу и конечно один светодиод не светится
+
+    /* w[x -:y] == w[x:(x-y+1)], $left возвращает индекс страшего бита
+    сигнал на led меняется практически мгновенно после обновления cnt.
+    Это можно представить как лампочку (led),
+    соединенную проводом (assign) с переключателем (cnt)
+    Таким образом можно настроить частоту мигания светодиодов */
+
+    //------------------------------------------------------------------------
 
     // Exercise 2: Key-controlled counter.
     // Comment out the code above.
@@ -97,8 +128,6 @@ module lab_top
     //
     // 2. Two counters controlled by different keys
     // displayed in different groups of LEDs.
-
-    /*
 
     wire any_key = | key;
 
@@ -122,6 +151,80 @@ module lab_top
 
     assign led = w_led' (cnt);
 
-    */
+
+    // 1. One key is used to increment, another to decrement.
+
+    wire inc_key = |key[0];
+    wire dec_key = |key[1];
+
+    logic inc_key_r, dec_key_r;
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+        begin
+            inc_key_r <= '0;
+            dec_key_r <= '0;
+        end
+        else
+        begin
+            inc_key_r <= inc_key;
+            dec_key_r <= dec_key;
+        end
+
+    wire inc_key_pressed = ~ inc_key & inc_key_r;
+    wire dec_key_pressed = ~ dec_key & dec_key_r;
+
+    logic [w_led - 1:0] cnt;
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+            cnt <= '0;
+        else if (inc_key_pressed)
+            cnt <= cnt + 1'd1;
+        else if (dec_key_pressed)
+            cnt <= cnt - 1'd1;
+
+    assign led = w_led' (cnt);
+
+    // 2. Two counters controlled by different keys
+    // displayed in different groups of LEDs.
+
+    wire key_0 = |key[0];
+    wire key_1 = |key[1];
+
+    logic key_0_r, key_1_r;
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+        begin
+            key_0_r <= '0;
+            key_1_r <= '0;
+        end
+        else
+        begin
+            key_0_r <= key_0;
+            key_1_r <= key_1;
+        end
+
+    wire key_0_pressed = ~ key_0 & key_0_r;
+    wire key_1_pressed = ~ key_1 & key_1_r;
+
+    logic [1:0] cnt0;
+    logic [1:0] cnt1;
+
+    always_ff @ (posedge clk or posedge rst)
+        if (rst)
+        begin
+            cnt0 <= '0;
+            cnt1 <= '0;
+        end
+        else if (key_0_pressed)
+            cnt0 <= cnt0 + 1'd1;
+        else if (key_1_pressed)
+            cnt1 <= cnt1 + 1'd1;
+
+    assign led = w_led' ({cnt1, cnt0});
+
+
 
 endmodule
